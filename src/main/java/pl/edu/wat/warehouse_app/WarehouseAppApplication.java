47e96be8018c420e3540a924129e82d7735280e1
Tomasz Lekenta <lekenta95@gmail.com>
2018-06-08 11:21:48 +0200

    Init

---
+++ src/main/java/pl/edu/wat/warehouse_app/WarehouseAppApplication.java
@@ -0,0 +1,12 @@
+package pl.edu.wat.warehouse_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarehouseAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WarehouseAppApplication.class, args);
+    }
+}
