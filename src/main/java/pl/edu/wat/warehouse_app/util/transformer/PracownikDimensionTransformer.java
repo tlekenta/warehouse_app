d0eb88f6d497f3004aec53c921a9067240eb0ef7
Tomek <Lekenta>
2018-06-11 22:31:36 +0200

    Zmiana ustalania id adresu w PracownikDimensionTransformer

--- src/main/java/pl/edu/wat/warehouse_app/util/transformer/PracownikDimensionTransformer.java
+++ src/main/java/pl/edu/wat/warehouse_app/util/transformer/PracownikDimensionTransformer.java
@@ -2,10 +2,13 @@ package pl.edu.wat.warehouse_app.util.transformer;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import pl.edu.wat.warehouse_app.stage.model.SourceToStageIdMap;
 import pl.edu.wat.warehouse_app.stage.model.warehouse.Stage_W_Adres;
 import pl.edu.wat.warehouse_app.stage.model.warehouse.Stage_W_Pracownik;
 import pl.edu.wat.warehouse_app.stage.model.zrodlo_system.Stage_Adres;
 import pl.edu.wat.warehouse_app.stage.model.zrodlo_system.Stage_Pracownik;
+import pl.edu.wat.warehouse_app.stage.repository.SourceToStageIdMapRepository;
+import pl.edu.wat.warehouse_app.stage.repository.StageToWarehouseIdMapRepository;
 import pl.edu.wat.warehouse_app.stage.repository.warehouse.Stage_W_AdresRepository;
 import pl.edu.wat.warehouse_app.stage.repository.warehouse.Stage_W_PracownikRepository;
 import pl.edu.wat.warehouse_app.stage.repository.zrodlo_system.Stage_AdresRepository;
@@ -14,7 +17,6 @@ import pl.edu.wat.warehouse_app.util.DbLogger;
 import pl.edu.wat.warehouse_app.util.ReflectionUtils;
 
 import java.sql.Timestamp;
-import java.text.MessageFormat;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -34,6 +36,10 @@ public class PracownikDimensionTransformer {
 
     ReflectionUtils reflectionUtils;
 
+    SourceToStageIdMapRepository sourceToStageIdMapRepository;
+
+    StageToWarehouseIdMapRepository stageToWarehouseIdMapRepository;
+
     DbLogger logger;
 
     public void transform() throws IllegalAccessException {
@@ -51,7 +57,7 @@ public class PracownikDimensionTransformer {
 
                 reflectionUtils.transformFields(newWorker, workerToSave);
 
-                workerToSave.setAdresId(getWarehouseAddress(newWorker.getAdresID()).getAdresId());
+                workerToSave.setAdresId(getWarehouseAddressId(newWorker.getAdresID()));
 
                 workerToSave.setTimestampTo(newWorker.getTimestampTo());
                 workerToSave.setTimestampFrom(newWorker.getTimestampFrom());
@@ -67,17 +73,13 @@ public class PracownikDimensionTransformer {
 
     }
 
-    private Stage_W_Adres getWarehouseAddress(Long sourceAddressId) {
-        Stage_Adres sourceAddress = stage_adresRepository.findOne(sourceAddressId);
-        return stage_w_adresRepository.findByUlicaAndNumerDomuAndNumerMieszkaniaAndKodPocztowyAndMiastoAndPoczta(
-                sourceAddress.getUlica(),
-                sourceAddress.getNumerBudynku(),
-                sourceAddress.getNumerLokalu(),
-                sourceAddress.getKodPocztowy(),
-                sourceAddress.getMiasto(),
-                sourceAddress.getPoczta()
-        );
+    private Long getWarehouseAddressId(Long sourceAddressId) {
+        SourceToStageIdMap sourceAddressMap = sourceToStageIdMapRepository.
+                findBySourceIdAndSourceTableName(sourceAddressId, "ZrodloSystem_Adres");
 
+        return stageToWarehouseIdMapRepository.
+                findByStageIdAndStageTableName(sourceAddressMap.getStageId(), sourceAddressMap.getStageTableName()).
+                getWarehouseId();
     }
 
 }
